259112e94af2f9a9b95e0943f038b3ddbc06db00
Villányi Máté László <m.villanyi97@gmail.com>
2021-04-05 20:24:02 +0200

    project initialized

---
+++ app/src/main/java/hu/vm/bme/skylordsauctions/cardlist/CardListActivity.kt
@@ -0,0 +1,13 @@
+package hu.vm.bme.skylordsauctions.cardlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.vm.bme.skylordsauctions.R
+
+class CardListActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
