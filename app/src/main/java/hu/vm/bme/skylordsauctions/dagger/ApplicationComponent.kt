c410b7c5bdbcea2bf1bc004700d0b808737aba60
Villányi Máté László <m.villanyi97@gmail.com>
2021-04-05 21:46:17 +0200

    add auction skeleton

--- app/src/main/java/hu/vm/bme/skylordsauctions/dagger/ApplicationComponent.kt
+++ app/src/main/java/hu/vm/bme/skylordsauctions/dagger/ApplicationComponent.kt
@@ -1,6 +1,7 @@
 package hu.vm.bme.skylordsauctions.dagger
 
 import dagger.Component
+import hu.vm.bme.skylordsauctions.addauction.AddAuctionActivity
 import hu.vm.bme.skylordsauctions.auctions.AuctionsActivity
 import hu.vm.bme.skylordsauctions.carddetails.CardDetailsActivity
 import hu.vm.bme.skylordsauctions.cardlist.CardListActivity
@@ -12,4 +13,5 @@ interface ApplicationComponent {
     fun inject(cardListActivity: CardListActivity)
     fun inject(cardDetailsActivity: CardDetailsActivity)
     fun inject(auctionsActivity: AuctionsActivity)
+    fun inject(addAuctionActivity: AddAuctionActivity)
 }
